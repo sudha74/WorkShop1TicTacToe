7637310dd928c51db2cd58e2b463554b18e7b952
sudha123 <sudhanaukala1997@gmail.com>
2022-08-16 19:29:07 +0530

    showBoard

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -27,9 +27,16 @@ public class TicTacToe {
         System.out.println("Computer Letter " + computerLetter);
     }
 
+    static void showBoard() {
+        System.out.println(board[1] + "|" + board[2] + "|" + board[3]);
+        System.out.println(board[4] + "|" + board[5] + "|" + board[6]);
+        System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
+    }
+
     public static void main(String[] args) {
         createBoard(board);
         getLetter();
+        showBoard();
 
     }
 
